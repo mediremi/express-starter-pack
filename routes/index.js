50b9e8fa509251e71181db72cee9308bcb29c3be
Médi-Rémi Hashim <mediremi@gmail.com>
2015-01-23 16:55:08 +0000

    Removed duplicate code

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,4 @@
-var nunjucks = require("nunjucks")
-
 module.exports = function(app) {
-	nunjucks.configure("views", {
-		autoescape: true,
-		express: app
-	})
-
 	app.get("/", function(req, res) {
 		res.render("index.html")
 	})
